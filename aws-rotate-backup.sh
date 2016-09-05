a283d1c41bb4a29d7b3d144f9ec6b6f73e4f97e6
Michael Pearce <pogomaniac@hotmail.com>
2016-09-05 14:43:53 +0100

    Update script to fix issue 1  - tag rotation 1
    Added mechanism to get id and new name of images as they are created, add them to array then tag the new image as rotation 1.

--- aws-rotate-backup.sh
+++ aws-rotate-backup.sh
@@ -1,86 +1,116 @@
 #!/bin/bash
 
-configlocation="~/.aws/config"
-user_profiles=($(cat $configlocation | grep 'profile ' |awk '{ print $2 ;}'|sed 's/]//'))
+configlocation="$HOME/.aws/config"
+user_profiles=($( cat $configlocation | grep 'profile ' |awk '{ print $2 ;}'|sed 's/]//'))
+
+NewImageNames=()
+NewImageIDs=()
+
 if [[ -z "${user_profiles[@]}" ]]; then
-echo "No user profiles found. Have you configured using 'aws configure' ?"
-echo "The script looked for the config file at $configlocation"
-echo "Exiting."
-exit
+    echo "No user profiles found. Have you configured using 'aws configure' ?"
+    echo "The script looked for the config file at $configlocation"
+    echo "Exiting."
+    exit
 fi
 
 echo "Which profile would you like to backup?"
 COUNTER=0
 for i in "${!user_profiles[@]}"
 do
-        printf '%s\n' "${user_profiles[$i]}"" - Press $COUNTER" 
-        COUNTER=$[COUNTER + 1]
+    printf '%s\n' "${user_profiles[$i]}"" - Press $COUNTER" 
+    COUNTER=$[COUNTER + 1]
 done
 read choice
 chosenprofile="${user_profiles[$choice]}"
 
 if [[ -z "$chosenprofile" ]];
 then echo 'Invalid selection. Exiting'
-exit
+     exit
 fi
 
 BackupInstanceIDS=($(aws ec2 describe-instances --profile $chosenprofile --output table --filters 'Name=tag:BACKUP,Values=True' | grep 'i-' | grep -v 'ami-\|eni-\|aki-' | awk  '{ print $4 ; }'))
 BackupInstanceNAMES=($(aws ec2 describe-instances --profile $chosenprofile --output table --filters 'Name=tag:BACKUP,Values=True' | grep 'Name'|grep -v 'GroupName\|KeyName\|PrivateDnsName\|PublicDnsName\|RootDeviceName\|DeviceName\|running' | awk '{ print $4" "$5; }'|sed 's/|//g'))
 
 if  [[ -z "${BackupInstanceIDS[@]}" ]]; then
-echo "No images tagged for backup, exiting"
-exit
+    echo "No images tagged for backup, exiting"
+    exit
 fi
+
 
 FourthRotationsIDS=($(aws ec2 describe-images --profile $chosenprofile --output table --filters 'Name=tag:Rotation,Values=4' |grep 'ami-' | awk  '{ print $4 ; }'))
 if [[ -z "${FourthRotationsIDS[@]}" ]];
 then echo "No fourth rotations to deregister."
 else
-for i in "${FourthRotationsIDS[@]}"
-do
-  echo "Deregistering Instance ID: $i"
-  aws ec2 deregister-image --profile $chosenprofile --image-id $i
-  echo "Done."
-done
+    for i in "${FourthRotationsIDS[@]}"
+    do
+	echo "Deregistering Instance ID: $i"
+	aws ec2 deregister-image --profile $chosenprofile --image-id $i
+	echo "Done."
+    done
 fi
 
 echo "Re-tagging Rotation 3s to 4.."
 ThreeToFourRotationsIDS=($(aws ec2 describe-images --profile $chosenprofile --output table --filters 'Name=tag:Rotation,Values=3' |grep 'ami-' | awk  '{ print $4 ; }'))
 for i in "${ThreeToFourRotationsIDS[@]}"
 do
-	echo "AMI ID: $i"
-        aws ec2 create-tags --profile $chosenprofile --resources  $i --tag Key=Rotation,Value=4
-        echo "Done."
+    echo "AMI ID: $i"
+    aws ec2 create-tags --profile $chosenprofile --resources  $i --tag Key=Rotation,Value=4
+    echo "Done."
 done
 
 echo "Re-tagging Rotation 2s to 3.."
 TwoToThreeRotationsIDS=($(aws ec2 describe-images --profile $chosenprofile --output table --filters 'Name=tag:Rotation,Values=2' |grep 'ami-' | awk  '{ print $4 ; }'))
 for i in "${TwoToThreeRotationsIDS[@]}"
 do
-        echo "AMI ID: $i"
-        aws ec2 create-tags --profile $chosenprofile --resources  $i --tag Key=Rotation,Value=3
-        echo "Done."
+    echo "AMI ID: $i"
+    aws ec2 create-tags --profile $chosenprofile --resources  $i --tag Key=Rotation,Value=3
+    echo "Done."
 done
 
 echo "Re-tagging Rotation 1s to 2.."
 OneToTwoRotationsIDS=($(aws ec2 describe-images --profile $chosenprofile --output table --filters 'Name=tag:Rotation,Values=1' |grep 'ami-' | awk  '{ print $4 ; }'))
 for i in "${OneToTwoRotationsIDS[@]}"
 do
-        echo "AMI ID: $i"
-        aws ec2 create-tags --profile $chosenprofile --resources  $i --tag Key=Rotation,Value=2
-        echo "Done."
+    echo "AMI ID: $i"
+    aws ec2 create-tags --profile $chosenprofile --resources  $i --tag Key=Rotation,Value=2
+    echo "Done."
 done
+
 echo "Creating latest image of instances.."
 
 NameCounter=0
 now="$(date +%d_%b_%Y)"
 for i in "${BackupInstanceIDS[@]}"
 do
-        echo "Creating image of instance ID: $i"
-        echo "Naming image: ${BackupInstanceNAMES[$NameCounter]}_BACKUP_$now"
-        aws ec2 create-image --instance-id  $i --no-reboot --profile $chosenprofile --name ${BackupInstanceNAMES[$NameCounter]}_BACKUP_$now
-        ((NameCounter++))
-        echo "Done."
+    echo "Creating image of instance ID: $i"
+    echo "Naming image: ${BackupInstanceNAMES[$NameCounter]}_BACKUP_$now"
+    aws ec2 create-image --instance-id  $i --no-reboot --profile $chosenprofile --name ${BackupInstanceNAMES[$NameCounter]}_BACKUP_$now
+    read -t 300 -p "Going to sleep to make sure instance gets created before trying to fetch name. To skip press ENTER" 
+    NewImageNames+=(${BackupInstanceNAMES[$NameCounter]}_BACKUP_$now)
+
+    ((NameCounter++))
+    echo "\nDone."
+done
+
+echo "added new image IDs to array"
+for i in "${NewImageNames[@]}"
+do
+    echo "Image Name: $i"
+    echo "Image ID: "
+    aws ec2 describe-images --profile $chosenprofile --output table --filters 'Name=name,Values='$i'' |grep 'ami-'|awk '{ print $4 ; }'
+
+    NewImageIDs+=($(aws ec2 describe-images --profile $chosenprofile --output table --filters 'Name=name,Values='$i'' |grep 'ami-'|awk '{ print $4 ; }'))
+done
+echo "printing NewImageIDs array (debug)"
+printf '%s\n' "${NewImageIDs[@]}"
+
+echo "Tagging new images as rotation 1.."
+
+for i in "${NewImageIDs[@]}"
+do
+    echo "AMI ID: $i"
+    aws ec2 create-tags --profile $chosenprofile --resources $i --tag Key=Rotation,Value=1
+    echo "Done."
 done
 
 echo "Backup rotations for $chosenprofile done."
